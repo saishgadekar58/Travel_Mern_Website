d38c441cc451a955730160c7b3d9c949acf3c152
saish gadekar <saish.gadekar.58@gmail.com>
2021-07-21 19:51:03 +0530

    2ndcommit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const User = require("./models/user");
+const { auth } = require("./middleware/auth");
 const config = require("./config/key");
 
 mongoose
@@ -19,16 +20,55 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get("/api/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+  });
 });
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
-  user.save((err, user) => {
+  user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
-    return res.status(200).json({
+    res.status(200).json({
       success: true,
+      userData: doc,
+    });
+  });
+});
+
+app.post("/api/user/login", (req, res) => {
+  //find the email
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSucess: false,
+        message: "Auth failed",
+      });
+
+    //comparepasswords
+
+    user.comparePassword(req.body, password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "invalid credentials",
+        });
+      }
+    });
+    //generatetoken
+
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+      });
     });
   });
 });
